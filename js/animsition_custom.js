6f1e5f483663a8e464d97281e9285c623809dd66
hanby-uxengineer <yihaanstar@gmail.com>
2022-02-03 23:48:30 +0900

    edit layout

--- js/animsition_custom.js
+++ js/animsition_custom.js
@@ -22,3 +22,23 @@ $(document).ready(function() {
     transition: function(url){ window.location.href = url; }
   });
 });
+
+
+function colorHandler(self) {
+  const slide = document.querySelector(".animsition-overlay-slide");
+
+  // slide.style.transitionDelay = ".5s";
+  slide.style.transitionDuration = "2s";
+
+  if(self.id == "color-home"){
+    slide.style.backgroundColor = "white";
+  }else if(self.id == "color-work"){
+    slide.style.backgroundColor = "#f0c8b6";
+  }else if(self.id == "color-about"){
+    slide.style.backgroundColor = "#cbb0ab";
+  }else if(self.id == "color-blog"){
+    slide.style.backgroundColor = "#817f95";
+  }else if(self.id == "color-sayhello"){
+    slide.style.backgroundColor = "#5c678a";
+  }
+}
