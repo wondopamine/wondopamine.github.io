b5a04c9cb399effa92842f47d22fa1674d0d404c
hanby-uxengineer <yihaanstar@gmail.com>
2022-02-08 22:50:05 +0900

    home ui

--- js/scroll_zoom.js
+++ js/scroll_zoom.js
@@ -1,44 +1,59 @@
-let lastScrollTop = 0;
-let scrollBoosterD = 2;
-let scrollBoosterU = 2;
+$(document).ready(function() {
+  let lastScrollTop = 0;
+  const p01Top = $('.p01').offset().top;
+  const p02Top = $('.p02').offset().top;
+  const p03Top = $('.p03').offset().top;
+  const p01Bottom = $('.p01End').offset().top + ($('.p01End').height() - $(window).height()) / 2; // container.top + (container.height - window.height) / delay_bumper
+  const p02Bottom = $('.p02End').offset().top + ($('.p02End').height() - $(window).height()) / 2;
+  const p03Bottom = $('.p03End').offset().top + ($('.p03End').height() - $(window).height()) / 2;
 
-$(window).scroll(function(event) {
-  const st = $(this).scrollTop();
-  if (st > lastScrollTop) {
-    scrollBoosterD = 2;    // scroll down
-    scrollBoosterU = 1;
-  } else {
-    scrollBoosterD = 1;  // scroll up
-    scrollBoosterU = 2;
-  }
-  lastScrollTop = st;
+  $(window).scroll(function(event) {
+    const scrollTop = $(this).scrollTop();
+    let brightnessVal = 50;
+    let borderWidthVal = 0;
 
-  const scrollTop = $(this).scrollTop();
-  const windowHeight = $(this).innerHeight();
-  const elementMarginTop = $('.container').offset().top - $('.main-description').innerHeight() - $('header').innerHeight();
-
-  $('.full-screen-sticky').each(function() {
-    if($(this).offset().top == $(window).scrollTop()) {
-      const elementHeight = $($(this).parent().children('.container')).innerHeight();
-      const elementTop = $($(this).parent().children('.container')).offset().top;
-      const stickyStart = elementTop - elementMarginTop;
-
-      $(this).css({
-        borderWidth: function() {
-          let border = scrollBoosterU * (30 - 30 * ((scrollBoosterD * (scrollTop-stickyStart)) / (elementHeight+elementTop-windowHeight-stickyStart)));
-          if(border < -1) {border = -1;}
-          if(border > 30) {border = 30;}
-          console.log(border);
-          return border + 'px';
-        }
-      });
-
-      $(this).children('.project-image').css({
-        filter: function() {
-          const brightness = 100 - 75 * ((scrollTop-stickyStart) / (elementHeight+elementTop-windowHeight-stickyStart));
-          return 'brightness(' + brightness + '%)';
-        }
-      });
+    if (scrollTop > lastScrollTop) {   // scroll down
+      brightnessVal = 50;
+      borderWidthVal = 0;
+      if(scrollTop >= p01Top) {
+        $('.p01').children('.project-image').css('filter', 'brightness('+ brightnessVal +'%)');
+        $('.p01').css('border-width', borderWidthVal);
+      } if(scrollTop >= p02Top) {
+        $('.p02').children('.project-image').css('filter', 'brightness('+ brightnessVal +'%)');
+        $('.p02').css('border-width', borderWidthVal);
+      } if(scrollTop >= p03Top) {
+        $('.p03').children('.project-image').css('filter', 'brightness('+ brightnessVal +'%)');
+        $('.p03').css('border-width', borderWidthVal);
+      }
+    } else {  // scroll up
+      brightnessVal = 100;
+      borderWidthVal = 50;
+      if(scrollTop <= p01Bottom) {
+        $('.p01').children('.project-image').css('filter', 'brightness('+ brightnessVal +'%)');
+        $('.p01').css('border-width', borderWidthVal);
+      } if(scrollTop <= p02Bottom) {
+        $('.p02').children('.project-image').css('filter', 'brightness('+ brightnessVal +'%)');
+        $('.p02').css('border-width', borderWidthVal);
+      } if(scrollTop <= p03Bottom) {
+        $('.p03').children('.project-image').css('filter', 'brightness('+ brightnessVal +'%)');
+        $('.p03').css('border-width', borderWidthVal);
+      }
     }
+    lastScrollTop = scrollTop;
+
+    // if(scrollTop >= p01Top && scrollTop <= p01Bottom) {
+    //   $('.p01').children('.project-image').css('filter', 'brightness('+ brightnessVal +'%)');
+    //   $('.p01').css('border-width', borderWidthVal);
+    // } else if(scrollTop >= p02Top && scrollTop <= p02Bottom) {
+    //   $('.p02').children('.project-image').css('filter', 'brightness('+ brightnessVal +'%)');
+    //   $('.p02').css('border-width', borderWidthVal);
+    // } else if(scrollTop >= p03Top && scrollTop <= p03Bottom) {
+    //   $('.p03').children('.project-image').css('filter', 'brightness('+ brightnessVal +'%)');
+    //   $('.p03').css('border-width', borderWidthVal);
+    // }
   });
 });
+
+function toTop() {
+  window.scrollTo({top:0, left:0, behavior:'smooth'});
+}
