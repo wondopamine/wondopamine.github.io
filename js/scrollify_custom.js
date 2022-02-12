447f1c4ab4deae986b4ad6007ebb8e3fd50202bf
hanby-uxengineer <yihaanstar@gmail.com>
2022-02-13 01:32:15 +0900

    work page ui

--- js/scrollify_custom.js
+++ js/scrollify_custom.js
@@ -1,6 +1,6 @@
 $(function() {
   $.scrollify({
-    section : "section",
+    section : "section"
   });
 });
 
@@ -9,3 +9,16 @@ function scrollifyTop() {
 }
 
 // $.scrollify.update() when the accordions open or close.
+
+$(document).scroll(function () {
+   $('section').each(function () {
+       if($(this).position().top <= $(document).scrollTop() && ($(this).position().top + $(this).outerHeight()) > $(document).scrollTop()) {
+           // console.log($(this).attr('id'));
+           if($(this).attr('id') == 'panel-2') {
+             $('header').css('background-color', '#00581B');
+           } else {
+             $('header').css('background-color', 'black');
+           }
+       }
+   });
+});
