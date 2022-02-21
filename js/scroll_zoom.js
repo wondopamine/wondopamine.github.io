dad534779d7e253ce630befb25f35301ef4eabe9
hanby-uxengineer <yihaanstar@gmail.com>
2022-02-21 10:23:54 +0900

    home > resize width only

--- js/scroll_zoom.js
+++ js/scroll_zoom.js
@@ -1,12 +1,26 @@
 $(document).ready(function() {
   let lastScrollTop = 0;
+  let windowWidth = $(window).width();
+  $(window).resize(function() {
+    if(windowWidth != $(window).width()) {
+      $(this).scrollTop(0);
+      windowWidth = $(window).width()
+    }
+    if($(this).width() >= 600) {
+      $('.project-section').find('.project-thumbnail').css('border-radius', 30 +'px');
+      $('.project-section').find('.scroll-zoom').css('border-width', 4.25 +'em');  /* scroll-zoom-variables */
+      $('.project-section').find('.project-title').css('margin-left', 4.25 +'em'); /* scroll-zoom-variables */
+    } else {
+      $('.project-section').find('.project-thumbnail').css('border-radius', 0 +'px');
+      $('.project-section').find('.scroll-zoom').css('border-width', 0 +'em');  /* scroll-zoom-variables */
+      $('.project-section').find('.project-title').css('margin-left', 0 +'em'); /* scroll-zoom-variables */
+    }
+  });
   $(window).scroll(function() {
     const scrollTop = $(this).scrollTop();
-    const windowHeight = $(window).height();
-    const windowWidth = $(window).height();
     $('.project-section').each(function(index, item) {
       const projectTop = $(this).offset().top;
-      const projectBottom = projectTop + (windowHeight * 1/2);
+      const projectBottom = projectTop + ($(this).height() * 1/2);
       if (scrollTop > lastScrollTop) {    // SCROLL DOWN
         if(scrollTop > projectTop) {
           $(this).find('.project-thumbnail').css('filter', 'brightness('+ 50 +'%)');
@@ -17,9 +31,15 @@ $(document).ready(function() {
       } else {                            // SCROLL UP
         if(scrollTop < projectBottom) {
           $(this).find('.project-thumbnail').css('filter', 'brightness('+ 100 +'%)');
-          $(this).find('.project-thumbnail').css('border-radius', 30 +'px');
-          $(this).find('.scroll-zoom').css('border-width', 4.25 +'em');  /* scroll-zoom-variables */
-          $(this).find('.project-title').css('margin-left', 4.25 +'em'); /* scroll-zoom-variables */
+          if($(this).width() >= 600) {
+            $(this).find('.project-thumbnail').css('border-radius', 30 +'px');
+            $(this).find('.scroll-zoom').css('border-width', 4.25 +'em');  /* scroll-zoom-variables */
+            $(this).find('.project-title').css('margin-left', 4.25 +'em'); /* scroll-zoom-variables */
+          } else {
+            $(this).find('.project-thumbnail').css('border-radius', 0 +'px');
+            $(this).find('.scroll-zoom').css('border-width', 0 +'em');  /* scroll-zoom-variables */
+            $(this).find('.project-title').css('margin-left', 0 +'em'); /* scroll-zoom-variables */
+          }
         }
       }
     });
